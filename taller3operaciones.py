47dce62298432111d165551aedd9a81d22bbfc62
mstorresh <mstorresh@unal.edu.co>
2018-09-28 22:21:23 -0500

    intentando la multiplicacion

--- taller3operaciones.py
+++ taller3operaciones.py
@@ -1,8 +1,8 @@
 #!/usr/bin/python
 # -*- coding: UTF-8 -*-
-a=(["+",5,8],[2,3,3,4])
-b=(["+",6,3,2],[1,8,3])
-def prim(a):
+a=(["+",7,3],[2,4])
+b=(["+",5],[1])
+def imprimir(a):
 	B , C =a[0]+["."]+a[1] , ""
 	for i in range(len(B)):
 		C += str(B[i])
@@ -40,7 +40,7 @@ def suma(a,b):
     a[0].insert(1,0)
     b[0].insert(1,0)
     c=([],[])
-    c[1].append(a[1][-1]+b[1][-1]%10)
+    c[1].append((a[1][-1]+b[1][-1])%10)
     for i in range(len(a[1])-1,0,-1): 
         c[1].insert(0,((a[1][i-1]+b[1][i-1])%10 +(a[1][i]+b[1][i])//10)%10)
     c[0].append(((a[0][-1]+b[1][-1]%10 + a[1][0]+b[1][-1]//10))%10)
@@ -49,9 +49,24 @@ def suma(a,b):
     c[0].insert(0,a[0][0])
     if c[0][1] ==0:
         c[0].pop(1)
-    prim (c)
+    imprimir (c)
     print (c)
 suma(a,b)	
-def resta(a,b):
-    if a[0][0] =="+" and b[0][0]=="-":
-        
+"""def resta(a,b):
+    if a[0][0] =="+" and b[0][0]=="-":"""
+def multiplicacion(a,b):
+    rellenar(a,b)
+    a[0].insert(1,0)
+    b[0].insert(1,0)
+    d=([],[])
+    e=[]
+    A=a[0]+a[1]
+    B=b[0]+b[1]
+    e.append((a[1][-1]*b[1][-1])%10)
+    for i in range(len(A)-1,1,-1):
+        for j in range(len(B)-1,1,-1):
+            e.insert(0,((A[i-1]*B[j-1])%10 +(A[i]*B[j])//10)%10)
+            print (A[i],B[j])
+    print(e)
+multiplicacion(a,b)
+    
