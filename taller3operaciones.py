d5bc5d166fc4e862830d693f449965d116ad5e1b
mstorresh <mstorresh@unal.edu.co>
2018-09-30 22:15:49 -0500

    ya esta con la funcion de comparar, falta la division

--- taller3operaciones.py
+++ taller3operaciones.py
@@ -1,9 +1,9 @@
 #!/usr/bin/python
 # -*- coding: UTF-8 -*-
-a=(["+",3],[2,4])
-b=(["-",5],[1])
-def imprimir(a):
-	B , C =a[0]+["."]+a[1] , ""
+a=(["+",5],[1,2])
+b=(["+",5],[1,2])
+def imprimir(x):
+	B , C =x[0]+["."]+x[1] , ""
 	for i in range(len(B)):
 		C += str(B[i])
 	print(C)
@@ -22,28 +22,45 @@ def rellenar(a,b):
 	elif len(b[0])>len(a[0]):
 		for i in range(len(b[0])-len(a[0])):
 			a[0].insert(1,0)
-def intlist(n): #transforma un enetero en una lista
+def intlist(n): #transforma un entero en una lista
     l = []
     while n != 0:
         l = [n % 10] + l
         n = n // 10
     return l
-#Desde esta parte es la forma de operar con las tuplas transformandolas a entero            
-def suma1(a,b):
+def tupstr(x): #transforma de tupla a string
     rellenar(a,b)
-    B , C =a[0]+a[1] , ""
+    B , C =x[0]+["."]+x[1] , ""
     for i in range(1,len(B)):
         C += str(B[i])
-    D , E =b[0]+b[1] , ""
-    for i in range(1,len(D)):
+    return C
+def tupint(x): #transforma de tupla a entero
+    rellenar(a,b)
+    B , C =x[0]+x[1] , ""
+    for i in range(1,len(B)):
+        C += str(B[i])
+    h = int(C)
+    return h 
+def tupflo(x): #transforma de tupla a float
+    D , E =x[0]+["."]+x[1] , ""
+    for i in range(len(D)):
         E += str(D[i])
-    c = int(C)
-    e = int(E)
+    p = float(E)
+    return p
+def listri(x,d): #transforma de lista a string
+ if d<len(x):
+  for u in range(len(x)): x[u]=str(x[u])
+  return (list('+'+str(''.join(x[:len(x)-d]))),list(str(''.join(x[len(x)-d:]))))
+ else: pass#
+    
+#Desde esta parte es la forma de operar con las tuplas transformandolas a entero            
+def suma1(a,b):
+    c=tupstr(a)
+    e=tupstr(b)
     s = c+e
     n = ([],[])
     l=intlist(s)
-    print(l)
-    #n[1].append(l[-1])
+    #print(l)
     for k in range(len(l)-1,len(a[0])-2,-1):
         n[1].insert(0,l[k]) 
         print(n[1])
@@ -58,15 +75,8 @@ def suma1(a,b):
 #print(73.243+5.1)
 #suma1(a,b)
 def resta1(a,b):
-    rellenar(a,b)
-    B , C =a[0]+a[1] , ""
-    for i in range(1,len(B)):
-        C += str(B[i])
-    D , E =b[0]+b[1] , ""
-    for i in range(1,len(D)):
-        E += str(D[i])
-    c = int(C)
-    e = int(E)
+    c=tupint(a)
+    e=tupint(b)
     s = c-e
     #print(s)
     if c>e:
@@ -106,19 +116,16 @@ def multiplicacion1(a,b):
     a1=len(a[1])
     b0=len(a[0])
     b1=len(a[1])
-    rellenar(a,b)
-    B , C =a[0]+a[1] , ""
-    for i in range(1,len(B)):
-        C += str(B[i])
-    D , E =b[0]+b[1] , ""
-    for i in range(1,len(D)):
-        E += str(D[i])
-    c = int(C)
-    e = int(E)
+    c=tupint(a)
+    e=tupint(b)
     s = c*e
+    print(s)
     if a[0][0]==b[0][0]:
         n=([],[])
         l=intlist(s)
+        if a[1][-1] == 0 or b[1][-1]==0:
+            if l[-1]==0:
+                l.pop(-1)
         print(l)
         for k in range(len(l)-1,(len(l)-(a1+b1)),-1):
             n[1].insert(0,l[k])
@@ -136,19 +143,24 @@ def multiplicacion1(a,b):
             e = e*(-1)
         n=([],[])
         l=intlist(s)
+        if a[1][-1] == 0 or b[1][-1]==0:
+            if l[-1]==0:
+                l.pop(-1)
         print(l)
         for k in range(len(l)-1,(len(l)-(a1+b1)),-1):
             n[1].insert(0,l[k])
         l.reverse()
-        for i in range(len(l)-1,(a1+b1)-1,-1):
+        for i in range(len(l)-1,(a1+b1)-2,-1):
             n[0].insert(0,l[i])
         n[0].reverse()
         n[0].insert(0,"-")
         #print(s)    
         print(n)
+#print(73.243*5.12)
+#multiplicacion1(a,b)
+#def division1(a,b):
+    
 #Desde esta parte es la forma de operar con la tuplas sin transformarlo a entero
-print(3.24*5.1)
-multiplicacion1(a,b)
 def suma(a,b):	
     rellenar(a,b)
     a[0].insert(1,0)
@@ -169,17 +181,42 @@ def suma(a,b):
 """def resta(a,b):
     if a[0][0] =="+" and b[0][0]=="-":"""
 """def multiplicacion(a,b):
+    q=str(tupint(a))
+    p=str(tupint(b))
+    A=list(q)
+    B=list(p)
+    D,d,f,g=[],[],0,0
+    for k in range(len(A)-1,-1-1):
+        for k2 in range(len(B)-1,-1,-1):
+            if int(A[k])*int(B[k2])+f<10: d.insert(0,int(A[k])*int(B[k2])+f);f=0
+            else: 
+                d.insert(0,int(str(int(A[k])*int(B[k2])+f)[1]))
+                f=int(str(int(A[k])*int(B[k2])+f)[0])
+            if k2==0 and f!=0: d.insert(0,f); f=0
+        D.append(d+g*["0"])
+        d,g=[],g+1
+    for k3 in range(len(D)):
+        d.insert(0,listri(D[k3]),len(b[1]))
+    M=d[0]
+    for k4 in range(len(M)):
+        M=suma(M,d[k4])
+    F=listri(M[0][1:]+D[1],len(a[1])+len(b[1]))
+    return (print(F))
+        
+print (73.245*5.12)
+multiplicacion(a,b)
+"""
+def comparacion(a,b):
     rellenar(a,b)
-    a[0].insert(1,0)
-    b[0].insert(1,0)
-    d=([],[])
-    e=[]
-    A=a[0]+a[1]
-    B=b[0]+b[1]
-    e.append((a[1][-1]*b[1][-1])%10)
-    for i in range(len(A)-1,1,-1):
-        for j in range(len(B)-1,1,-1):
-            e.insert(0,((A[i-1]*B[j-1])%10 +(A[i]*B[j])//10)%10)
-            print (A[i],B[j])
-    print(e)
-multiplicacion(a,b)"""
+    c=1
+    for i in range(len(a[0])):
+        if a[0][i] != b[0][i]:
+            c=0
+    for j in range(0,len(a[1])):
+        if a[1][j] != b[1][j]:
+            c=0
+    if c==1:
+        print("son iguales")
+    if c==0:
+        print("No son iguales")    
+comparacion(a,b)
