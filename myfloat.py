32086ecb61a595b5210721587beae2da850301cd
mstorresh <mstorresh@unal.edu.co>
2018-10-02 18:27:03 -0500

    finalizado, raro que no sirve el if name=main, y se demora muchoooooooooooooooooo en converger, segun wikipedia es muyyyyy lento

--- myfloat.py
+++ myfloat.py
@@ -1,7 +1,6 @@
 import myfloat_func as my
 class Decimales:
     def __init__(self, z): #parametro son las tuplas, enteros y decimales
-        self.z=z
         if (type(z) is int) or (type(z) is float):
             self.z=my.numtup(z)
         else:
@@ -11,25 +10,24 @@ class Decimales:
             other = my.numtup(other)
             return Decimales(my.suma(self.z,other))
         if isinstance(other, Decimales):
-            return Decimales(my.suma(self.z,other))
+            return Decimales(my.suma(self.z,other.z))
     def __sub__(self,other):
         if isinstance(other,(int,float)):
             other = my.numtup(other)
             return Decimales(my.resta1(self.z,other))
         if isinstance(other,Decimales):
-            return Decimales(my.resta1(self.z,other))
+            return Decimales(my.resta1(self.z,other.z))
     def __mul__(self,other):
         if isinstance(other,(int,float)):
             other = my.numtup(other)
             return Decimales(my.multiplicacion1(self.z,other))
         if isinstance(other,Decimales):
-            return Decimales(my.multiplicacion1(self.z,other))
+            return Decimales(my.multiplicacion1(self.z,other.z))
     def __truediv__(self,other):
-        if isinstance(other,(int,float)):
-            other = my.numtup(other)
-            return Decimales(my.division(self.z,other))
-        if isinstance(other,Decimales):
-            return Decimales(my.division(self.z,other))
+        if isinstance (other,Decimales):
+            return Decimales(my.division(self.z,other.z))
+        elif isinstance (other ,( int , float )):
+            return Decimales(my.division(self.z,my.numtup(other)))
     def __radd__(self,other):
         if isinstance(other,(int,float)):
             other = my.numtup(other)
@@ -49,11 +47,9 @@ class Decimales:
         if isinstance(other,Decimales):
             return Decimales(my.multiplicacion1(other,self.z))
     def __rtruediv__(self,other):
-        if isinstance(other,(int,float)):
-            other = my.numtup(other)
-            return Decimales(my.division(other,self.z))
-        if isinstance(other,Decimales):
-            return Decimales(my.division(other,self.z))
+        other = Decimales(other)
+        return other/self
+		
     def __str__(self):
         return my.imprimir(self.z)
     def __repr__(self):
@@ -68,6 +64,17 @@ class Decimales:
 if __name__=="main":
     pi=Decimales(0)
     for k in range(0,30000):
-        pi = pi + ((-1)**k)/(Decimales(2*k+1))
-    pi *= 4
+        pi = pi + Decimales(((-1)**k)/(Decimales(2*k+1)))
+    pi *=4
     print(pi)
+pi=Decimales(0)
+for k in range(0,100000000):
+    if k%2==0:
+        pi = pi + 1/(Decimales(2*k+1))
+    else:
+        pi = pi - 1/(Decimales(2*k+1))
+pi = pi*4
+for j in range(30,len(pi.z[1])):
+    pi.z[1].pop(-1)
+print(pi)
+    
